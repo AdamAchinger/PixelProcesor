8bddb2bac7da017f3490727c53d68b0f54676933
Adam Achinger <72527645+AdamAchinger@users.noreply.github.com>
2024-06-04 23:52:59 +0200

    Math function updatte

--- lib/Math.py
+++ lib/Math.py
@@ -3,24 +3,23 @@ from tkinter import *
 from tkinter import messagebox 
 
 ### Version
-toolVersion = 1.1
+toolVersion = 2.3
 ###
 
 def Math(inputOutput1):
-    Grad = Toplevel()
+    Math = Toplevel()
 
-    root = Grad
+    root = Math
 
     bgColor = "#353535"
     fgColor = "#C0C0C0"
     hlColor = "#777777"
     fontSizeSmall = 13
 
-
     root.configure(bg=bgColor)
     root.title("Math"+" v"+str(toolVersion))
     root.iconbitmap('S:\GitHub\PixelProcesor\img\AA_icon.ico')
-    root.resizable(False,False)
+    #root.resizable(False,False)
 
     #####
     # application dimensions
@@ -36,31 +35,87 @@ def Math(inputOutput1):
     root.geometry(f'{appWidth}x{appHeight}+{appXpos}+{appYpos}')
     #####
 
+
+
     def Generate():
         from PIL import Image
 
+        Multiply3 = inputMultiply.get()
+        Power3 = inputPower.get()
+        Add3 = inputAdd.get()
+        Subtract3 = inputSubtract.get()
+        Min3 = inputMin.get()
+        Max3 = inputMax.get()
+        InputFile = inputInputFile.get()    
+        Output = inputOutput.get()    
+        
+        img = Image.open(InputFile)
+        Width, Height = img.size
 
+        for w in range(Width):
+            for h in range(Height):
+
+                ### Multiply ###
+                if(Multiply3!="1,1,1"):
+                    R1,G1,B1, = img.getpixel((w,h))
+                    R = int(R1 * float(Multiply3.split(",")[0]))
+                    G = int(G1 * float(Multiply3.split(",")[1]))
+                    B = int(B1 * float(Multiply3.split(",")[2]))
+                    img.putpixel((w,h),(R,G,B))
+                ### Power ###
+                if(Power3!="1,1,1"):
+                    R1,G1,B1 = img.getpixel((w,h))
+                    R = int(pow(R1,float(Power3.split(",")[0])))
+                    G = int(pow(G1,float(Power3.split(",")[1])))
+                    B = int(pow(B1,float(Power3.split(",")[2])))
+                    img.putpixel((w,h),(R,G,B))
+
+                ### Add ###
+                if(Add3!="0,0,0"):
+                    R1,G1,B1 = img.getpixel((w,h))
+                    R = int(R1+(float(Add3.split(",")[0])*255))
+                    G = int(G1+(float(Add3.split(",")[1])*255))
+                    B = int(B1+(float(Add3.split(",")[2])*255))
+
+                    img.putpixel((w,h),(R,G,B))
+
+                ### Subtract ###
+                if(Subtract3!="0,0,0"):
+                    R1,G1,B1 = img.getpixel((w,h))
+                    R = int(R1-(float(Subtract3.split(",")[0])*255))
+                    G = int(G1-(float(Subtract3.split(",")[1])*255))
+                    B = int(B1-(float(Subtract3.split(",")[2])*255))
+                    img.putpixel((w,h),(R,G,B))
+
+                ### Clamp ###
+
+                rMin = float(Min3.split(",")[0])*255
+                gMin = float(Min3.split(",")[1])*255
+                bMin = float(Min3.split(",")[2])*255
+
+                rMax = float(Max3.split(",")[0])*255
+                gMax = float(Max3.split(",")[1])*255
+                bMax = float(Max3.split(",")[2])*255
             
+                R1,G1,B1 = img.getpixel((w,h))
 
+                R = int(min(max(R1, rMin), rMax))
+                G = int(min(max(G1, gMin), gMax))
+                B = int(min(max(B1, bMin), bMax))
+
+                img.putpixel((w,h),(R,G,B))
+
+        img.save(Output)
 
     #### Math #### 
     frame1 = Frame(root,width=100,bg=bgColor)
     frame1.grid(row=0,column=0,pady=10,padx=60,sticky=S)
 
     label_1 = Label(frame1,text="Math",font=("roboto",32),bg=bgColor,fg=fgColor)
-    label_1.grid(row=0,column=0,pady=10,padx=60,sticky=S)
+    label_1.grid(row=0,column=0,pady=3,padx=60,sticky=S)
 
-
-    #### Input File #### 
-    frame51 = Frame(root,width=100,bg=bgColor,highlightbackground=hlColor,highlightthickness=1)
-    frame51.grid(row=1,column=0,pady=6,padx=12,sticky=S)
-
-    labelInputFile = Label(frame51,text="INPUT FILE",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
-    labelInputFile.grid(row=1,column=0,sticky=S,pady=6,padx=16)
-
-    inputInputFile = Entry(frame51,width=40,font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
-    inputInputFile.grid(row=2,column=0,sticky=S,pady=2,padx=16)
-    inputInputFile.insert(0,"Import File")
+    label_2 = Label(frame1,text="RED,GREEN,BLUE",font=("roboto",12),bg=bgColor,fg=fgColor)
+    label_2.grid(row=1,column=0,pady=1,padx=5,sticky=S)
 
 
 
@@ -71,35 +126,27 @@ def Math(inputOutput1):
     labelMultiply = Label(frame21,text="MULTIPLY",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor).grid(row=1,column=0,pady=6,padx=16)
     inputMultiply = Entry(frame21,width=10,font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
     inputMultiply.grid(row=2,column=0,pady=2,padx=16)
-    inputMultiply.insert(0,"1")
+    inputMultiply.insert(0,"1,1,1")
 
     #### Power #### 
     frame22 = Frame(root,width=25,bg=bgColor,highlightbackground=hlColor,highlightthickness=1)
-    frame22.grid(row=3,column=0,pady=6,padx=147,sticky=W)
+    frame22.grid(row=4,column=0,pady=6,padx=12,sticky=W)
 
     labelPower = Label(frame22,text="POWER",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor).grid(row=1,column=0,pady=6,padx=16)
     inputPower = Entry(frame22,width=10,font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
     inputPower.grid(row=2,column=0,pady=2,padx=16)
-    inputPower.insert(0,"1")
+    inputPower.insert(0,"1,1,1")
 
-    #### Divide #### 
-    frame23 = Frame(root,width=25,bg=bgColor,highlightbackground=hlColor,highlightthickness=1)
-    frame23.grid(row=3,column=0,pady=6,padx=12,sticky=E)
-
-    labelDivide = Label(frame23,text="DIVIDE",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor).grid(row=1,column=0,pady=6,padx=16)
-    inputDivide = Entry(frame23,width=10,font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
-    inputDivide.grid(row=2,column=0,pady=2,padx=16)
-    inputDivide.insert(0,"1")
 
 
     #### Add #### 
     frame24 = Frame(root,width=25,bg=bgColor,highlightbackground=hlColor,highlightthickness=1)
-    frame24.grid(row=4,column=0,pady=6,padx=12,sticky=W)
+    frame24.grid(row=3,column=0,pady=6,padx=147,sticky=W)
 
     labelAdd = Label(frame24,text="ADD",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor).grid(row=1,column=0,pady=6,padx=16)
     inputAdd = Entry(frame24,width=10,font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
     inputAdd.grid(row=2,column=0,pady=2,padx=16)
-    inputAdd.insert(0,"0")
+    inputAdd.insert(0,"0,0,0")
 
     #### Subtract #### 
     frame25 = Frame(root,width=25,bg=bgColor,highlightbackground=hlColor,highlightthickness=1)
@@ -108,23 +155,45 @@ def Math(inputOutput1):
     labelSubtract = Label(frame25,text="SUBTRACT",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor).grid(row=1,column=0,pady=6,padx=16)
     inputSubtract = Entry(frame25,width=10,font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
     inputSubtract.grid(row=2,column=0,pady=2,padx=16)
-    inputSubtract.insert(0,"0")
+    inputSubtract.insert(0,"0,0,0")
 
-    #### Clamp #### 
+    #### Max #### 
     frame26 = Frame(root,width=25,bg=bgColor,highlightbackground=hlColor,highlightthickness=1)
-    frame26.grid(row=4,column=0,pady=6,padx=12,sticky=E)
+    frame26.grid(row=3,column=0,pady=6,padx=12,sticky=E)
 
-    labelClamp = Label(frame26,text="(MIN/MAX)",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor).grid(row=1,column=0,pady=6,padx=16)
-    inputClamp = Entry(frame26,width=10,font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
-    inputClamp.grid(row=2,column=0,pady=2,padx=16)
-    inputClamp.insert(0,"1,1")
+    labelMax = Label(frame26,text="MAX",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor).grid(row=1,column=0,pady=6,padx=16)
+    inputMax = Entry(frame26,width=10,font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
+    inputMax.grid(row=2,column=0,pady=2,padx=16)
+    inputMax.insert(0,"1,1,1")
+
+    #### Min #### 
+    frame27 = Frame(root,width=25,bg=bgColor,highlightbackground=hlColor,highlightthickness=1)
+    frame27.grid(row=4,column=0,pady=6,padx=12,sticky=E)
+
+    labelMin = Label(frame27,text="MIN",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor).grid(row=1,column=0,pady=6,padx=16)
+    inputMin = Entry(frame27,width=10,font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
+    inputMin.grid(row=2,column=0,pady=2,padx=16)
+    inputMin.insert(0,"0,0,0")
 
 
-    #### OUTPUT DIRECTORY #### 
+
+
+    #### INPUT PATH #### 
+    frame51 = Frame(root,width=100,bg=bgColor,highlightbackground=hlColor,highlightthickness=1)
+    frame51.grid(row=5,column=0,pady=6,padx=12,sticky=S)
+
+    labelInputFile = Label(frame51,text="INPUT FILE",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
+    labelInputFile.grid(row=1,column=0,sticky=S,pady=6,padx=16)
+
+    inputInputFile = Entry(frame51,width=40,font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
+    inputInputFile.grid(row=2,column=0,sticky=S,pady=2,padx=16)
+    inputInputFile.insert(0,inputOutput1)
+
+    #### OUTPUT PATH #### 
     frame52 = Frame(root,width=100,bg=bgColor,highlightbackground=hlColor,highlightthickness=1)
-    frame52.grid(row=5,column=0,pady=6,padx=12,sticky=S)
+    frame52.grid(row=6,column=0,pady=6,padx=12,sticky=S)
 
-    labelOutput = Label(frame52,text="OUTPUT DIRECTORY",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
+    labelOutput = Label(frame52,text="OUTPUT FILE RGBA",font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
     labelOutput.grid(row=1,column=0,sticky=S,pady=6,padx=16)
 
     inputOutput = Entry(frame52,width=40,font=("roboto",fontSizeSmall),bg=bgColor,fg=fgColor)
@@ -132,10 +201,10 @@ def Math(inputOutput1):
     inputOutput.insert(0,inputOutput1)
 
     #### Generate #### 
-    frame6 = Frame(root,width=100,bg=bgColor)
-    frame6.grid(row=7,column=0,pady=10,padx=60,sticky=S)
+    frame61 = Frame(root,width=100,bg=bgColor)
+    frame61.grid(row=7,column=0,pady=10,padx=60,sticky=S)
 
-    saveButton = Button(frame6,text="Save",font=("roboto",18),bg=bgColor,fg=fgColor,pady=2,padx=25,command=Generate)
+    saveButton = Button(frame61,text="Save",font=("roboto",18),bg=bgColor,fg=fgColor,pady=2,padx=25,command=Generate)
     saveButton.grid(row=0,column=0,sticky=S,pady=2,padx=2)
 
 
