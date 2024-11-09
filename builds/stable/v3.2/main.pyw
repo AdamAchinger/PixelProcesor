3076c8931d558b667a3732deca5806ebcca7dbb2
AdamAchinger <adamachinger123@gmail.com>
2024-11-09 14:40:57 +0100

    adding Stable Version

---
+++ builds/stable/v3.2/main.pyw
@@ -0,0 +1,50 @@
+from tkinter import *
+import customtkinter as ctk
+from module import solid, gradient, math
+import constants as c 
+
+#### Initialize Window ####
+root = ctk.CTk()
+ctk.set_default_color_theme(c.theme) 
+root.configure(bg=c.bgColor)
+root.title("Pixel Procesor " + str(c.toolVersion))
+root.iconbitmap(c.iconPath)
+root.resizable(FALSE, FALSE)
+screenWidth = root.winfo_screenwidth()
+screenHeight = root.winfo_screenheight()
+appXpos = int((screenWidth / 2) - (c.appWidth / 2))
+appYpos = int((screenHeight / 2) - (c.appHeight / 2))
+root.geometry(f'{c.appWidth}x{c.appHeight}+{appXpos}+{appYpos}')
+
+# Create the tab view (this is the equivalent of a tabbed frame)
+tab_view = ctk.CTkTabview(root)
+tab_view.pack(expand=True, fill="both")
+
+# Add tabs to the tab view
+tab1 = tab_view.add("Solid Color")
+tab2 = tab_view.add("Gradient")
+tab3 = tab_view.add("Math")
+#tab4 = tab_view.add("Separate")
+#tab5 = tab_view.add("Combine")
+#tab6 = tab_view.add("Mask")
+#tab7 = tab_view.add("Resize")
+#tab8 = tab_view.add("Color Picker")
+#tab8 = tab_view.add("Filter")
+
+my_font = ctk.CTkFont(size=18)  # Font object
+for button in tab_view._segmented_button._buttons_dict.values():
+    button.configure(height=32, font=my_font)  # Change font using font object
+
+
+
+# Initialize classes within the appropriate tabs
+solid_tab = solid.Solid(tab1)
+gradient_tab = gradient.Gradient(tab2)
+math_tab = math.Math(tab3)
+#separate_tab = separate.Separate(tab4)
+#combine_tab = combine.Combine(tab5)
+
+#mask_tab = mask.Mask(tab6)
+
+
+root.mainloop()
---
+++ builds/stable/v3.2/module/gradient.py
@@ -0,0 +1,177 @@
+import os
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image
+import customtkinter as ctk
+import constants as c 
+import utils as u 
+
+if __name__ != "__main__" : 
+    class Gradient:
+        def __init__(self, master):
+            self.img = Image.open(c.previewPath)     
+
+            leftTabFrame, frameTop, frameScroll, frameBottom = u.tabFrame(master)
+
+            #### Input ####
+            frame1 = ctk.CTkFrame(frameScroll)
+            frame1.pack()
+
+            frame11 = ctk.CTkFrame(frameScroll)
+            frame11.pack()
+
+            frame2 = ctk.CTkFrame(frameScroll)
+            frame2.pack()
+
+            frame3 = ctk.CTkFrame(frameScroll)
+            frame3.pack(padx=1,side=LEFT)
+
+            frame01 = ctk.CTkFrame(frame1, width=c.cellW2, height=c.cellH2-30)
+            frame01.pack(padx=2, pady=2, side=LEFT)
+            frame01.propagate(False)
+
+            frame011 = ctk.CTkFrame(frame11, width=c.cellW2, height=c.cellH2-30)
+            frame011.pack(padx=2, pady=2, side=LEFT)
+            frame011.propagate(False)
+
+
+            frame02 = ctk.CTkFrame(frame2, width=c.cellW, height=c.cellH)
+            frame02.pack(padx=2, pady=2, side=LEFT)
+            frame02.propagate(False)
+
+            frame03 = ctk.CTkFrame(frame2, width=c.cellW, height=c.cellH)
+            frame03.pack(padx=2, pady=2, side=LEFT)
+            frame03.propagate(False)
+
+            frame04 = ctk.CTkFrame(frame3, width=c.cellW, height=c.cellH)
+            frame04.pack(padx=2, pady=2, side=LEFT)
+            frame04.propagate(False)
+
+            frame05 = ctk.CTkFrame(frame3, width=c.cellW, height=c.cellH)
+            frame05.pack(padx=2, pady=2, side=LEFT)
+            frame05.propagate(False)
+
+            frame06 = ctk.CTkFrame(frameTop, width=400, height=30)
+            frame06.pack(padx=2, pady=2)
+            frame06.propagate(False)
+        
+
+            u.previewImage(self,leftTabFrame)
+
+            u.outputDir(self,frame06)
+            
+            u.previewMethod(self,frameBottom,DISABLED)
+
+
+            self.exportButton = ctk.CTkButton(frameBottom, text="Export",width=198, height=40,state=DISABLED,command=lambda: [u.export(self),u.update_preview(self)], font=c.bFont)
+            self.exportButton.pack(pady=2, padx=2, side=RIGHT)
+            
+            generateButton = ctk.CTkButton(frameBottom, text="Generate",width=198, height=40,command=lambda: [self.generate(),u.update_preview(self)], font=c.bFont)
+            generateButton.pack(pady=2, padx=2, side=RIGHT)
+
+            previewFrame = ctk.CTkFrame(frameBottom, width=50,fg_color="#2b2b2b")
+            previewFrame.pack(padx=8, pady=2, side=RIGHT)
+
+
+            #### Color A ####
+            labelColor = ctk.CTkLabel(frame01, text="Color A (RGB)", font=c.mFont)
+            labelColor.pack(pady=6, padx=16)
+            
+            self.AsliderRed = u.slider(frame01,"Red",FALSE,1.0)
+            self.AsliderGreen = u.slider(frame01,"Green",FALSE,1.0)
+            self.AsliderBlue = u.slider(frame01,"Blue",FALSE,1.0)
+            #self.AsliderAlpha = u.slider(frame01,"Alpha",TRUE)
+
+            #### Color B ####
+            labelColor = ctk.CTkLabel(frame011, text="Color B (RGB)", font=c.mFont)
+            labelColor.pack(pady=6, padx=16)
+            
+            self.BsliderRed = u.slider(frame011,"Red",FALSE,0.0)
+            self.BsliderGreen = u.slider(frame011,"Green",FALSE,0.0)
+            self.BsliderBlue = u.slider(frame011,"Blue",FALSE,0.0)
+            #self.BsliderAlpha = u.slider(frame01,"Alpha",TRUE)
+            
+            #### FILENAME ####
+            labelFilename = ctk.CTkLabel(frame02, text="File Name", font=c.mFont)
+            labelFilename.pack(pady=6, padx=16)
+
+            self.inputFilename = ctk.CTkEntry(frame02, width=c.entryWidth, font=c.mFont)
+            self.inputFilename.pack(pady=6, padx=16)
+            self.inputFilename.insert(0, "T_Gradient")
+
+            #### SIZE ####
+            labelSize = ctk.CTkLabel(frame03, text="Size", font=c.mFont)
+            labelSize.pack(pady=6, padx=16)
+
+            self.inputSizeWidth = ctk.CTkEntry(frame03, width=c.entryWidth/2, font=c.mFont)
+            self.inputSizeWidth.pack(pady=6, padx=8,side=LEFT)
+            self.inputSizeWidth.insert(0, "512")
+
+            labelSize = ctk.CTkLabel(frame03, text="x", font=c.mFont)
+            labelSize.pack(pady=6, padx=0,side=LEFT)
+
+            self.inputSizeHeight = ctk.CTkEntry(frame03, width=c.entryWidth/2, font=c.mFont)
+            self.inputSizeHeight.pack(pady=6, padx=8,side=LEFT)
+            self.inputSizeHeight.insert(0, "512")
+
+            #### FILETYPE ####
+            labelFiletype = ctk.CTkLabel(frame04, text="File Type", font=c.mFont)
+            labelFiletype.pack(pady=6, padx=16)
+
+            self.Filetype = ctk.StringVar()
+            self.Filetype.set(c.Extensions[0])
+            self.inputFiletype = ctk.CTkSegmentedButton(frame04,width=250,variable=self.Filetype, values=c.Extensions,  font=c.sFont)
+            self.inputFiletype.pack(pady=6, padx=16)
+
+            #### Orient ####
+            labelFiletype = ctk.CTkLabel(frame05, text="Orient", font=c.mFont)
+            labelFiletype.pack(pady=6, padx=16)
+
+            self.inputFlip = ctk.StringVar()
+            self.inputFlip.set(c.orient[0])
+            self.inputFlip = ctk.CTkSegmentedButton(frame05,width=250,variable=self.inputFlip, values=c.orient,  font=c.sFont)
+            self.inputFlip.pack(pady=6, padx=16)
+
+
+        
+
+        def generate(self):
+            self.exportButton.configure(state=NORMAL)
+
+            OutputDir = self.Path
+            Filename = self.inputFilename.get()
+            Filetype = self.inputFiletype.get().lower()
+
+            Orient = self.inputFlip.get()
+            Width = int(self.inputSizeWidth.get())
+            Height = int(self.inputSizeHeight.get())
+            
+            self.img = Image.new(mode="RGB", size=(Width, Height))
+
+            R1 = self.AsliderRed.get()
+            G1 = self.AsliderGreen.get()
+            B1 = self.AsliderBlue.get() 
+            R2 = self.BsliderRed.get()
+            G2 = self.BsliderGreen.get()
+            B2 = self.BsliderBlue.get() 
+
+            for w in range(Width):
+                for h in range(Height):
+                    if Orient == "H" or Orient == "H.F":
+                        v = int((h / Height) * 255) 
+                    else:
+                        v = int((w / Width) * 255)
+                    if Orient == "H.F" or Orient == "V.F":
+                        v = abs(v - 255)
+
+                    R = int(v * R2 + abs(v - 255) * R1)
+                    G = int(v * G2 + abs(v - 255) * G1)
+                    B = int(v * B2 + abs(v - 255) * B1)
+
+                    self.img.putpixel((w, h), (R, G, B))
+
+                    
+
+
+            self.full_path = os.path.join(OutputDir, Filename + "."+Filetype)
+        
